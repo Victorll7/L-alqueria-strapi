5ea27d226fad71980e208106f9f41e093586db1b
Victor <llinaresvictor7@gmail.com>
2024-07-29 01:21:21 +0200

    strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,15 @@
-module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "sendgrid",
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
-      settings: {
-        defaultFrom: "L´ Alqueria <victor_pro_@hotmail.com>",
-        defaultReplyTo: "L´ Alqueria <victor_pro_@hotmail.com>",
-        testAddress: "victor_pro_@hotmail.com",
-      },
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   email: {
+//     config: {
+//       provider: "sendgrid",
+//       providerOptions: {
+//         apiKey: env("SENDGRID_API_KEY"),
+//       },
+//       settings: {
+//         defaultFrom: "L´ Alqueria <victor_pro_@hotmail.com>",
+//         defaultReplyTo: "L´ Alqueria <victor_pro_@hotmail.com>",
+//         testAddress: "victor_pro_@hotmail.com",
+//       },
+//     },
+//   },
+// });
