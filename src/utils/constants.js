526b59d9257faeac02960158848d3be9ab4338ef
Victor <llinaresvictor7@gmail.com>
2024-07-31 11:59:16 +0200

    cambios

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const ENV = {
-  SERVER_HOST: "https://alqueriatest-production.up.railway.app/",
+  SERVER_HOST: "https://alqueriatest-production.up.railway.app",
   API_URL: "https://alqueriatest-production.up.railway.app/api",
   ENDPOINTS: {
     AUTH: {
