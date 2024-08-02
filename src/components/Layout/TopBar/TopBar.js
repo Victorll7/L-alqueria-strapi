0460fe4d389a004bf59785861c0c07f81f75fc42
Victor <llinaresvictor7@gmail.com>
2024-08-02 20:19:45 +0200

    new menu

--- src/components/Layout/TopBar/TopBar.js
+++ src/components/Layout/TopBar/TopBar.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Image } from "semantic-ui-react";
 import Link from "next/link";
 import { Account } from "../Account";
@@ -6,9 +7,25 @@ import styles from "./TopBar.module.scss";
 
 export function TopBar(props) {
   const { isOpenSearch, hideMenu } = props;
+  const [isTop, setIsTop] = useState(true); // Estado para controlar si estamos en la parte superior de la página
+
+  useEffect(() => {
+    // Función para manejar el scroll y determinar si estamos en la parte superior
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      setIsTop(scrollTop === 0);
+    };
+
+    // Agregar evento de scroll al montar el componente
+    window.addEventListener("scroll", handleScroll);
+
+    // Limpiar el evento de scroll al desmontar el componente para evitar fugas de memoria
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className={styles.topBar}>
+    <div className={`${styles.topBar} ${!isTop ? styles.scrolled : ""}`}>
       <div className={styles.left}>
         <Link href="/">
           <Image src="/images/logo-topbar-lalqueria.png" alt="L´alqueria" />
@@ -19,7 +36,7 @@ export function TopBar(props) {
         {!hideMenu && <Menu isOpenSearch={isOpenSearch} />}
       </div>
 
-      <div className={styles.right}>
+      <div className={`${styles.right} ${!isTop ? styles.hideRight : ""}`}>
         <Account />
       </div>
     </div>
